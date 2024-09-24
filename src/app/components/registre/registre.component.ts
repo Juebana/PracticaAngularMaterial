dc674c10063406c0d54a7a50ff570886a17481e4
Guanlin Wang <guanlin987@gmail.com>
2024-09-24 19:25:25 +0200

    first commit

---
+++ src/app/components/registre/registre.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registre',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './registre.component.html',
+  styleUrl: './registre.component.css'
+})
+export class RegistreComponent {
+
+}
