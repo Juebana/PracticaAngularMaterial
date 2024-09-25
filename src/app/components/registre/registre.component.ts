fcbe96452b86455ca7f8bfde68adc3873e335cf4
Guanlin Wang <guanlin987@gmail.com>
2024-09-25 16:56:34 +0200

    implemented the dni autocomplete functionality

--- src/app/components/registre/registre.component.ts
+++ src/app/components/registre/registre.component.ts
@@ -9,5 +9,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './registre.component.css'
 })
 export class RegistreComponent {
+  dni: string = '';
 
+  onDNIInput(): void {
+    if (this.dni.length === 8 && /^[0-9]+$/.test(this.dni)) {
+      const dniNumber = parseInt(this.dni, 10);
+      const letra = this.calculateDNILetter(dniNumber);
+      this.dni = `${this.dni}${letra}`;
+      const dniInput = document.querySelector('input[type="text"]') as HTMLInputElement;
+      dniInput.value = this.dni;
+    }
+  }
+
+  calculateDNILetter(dni: number): string {
+    const letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    return letras[dni % 23];
+  }
 }
